2190b46348d9c9c2f14af6c8570c1116c38a029c
Gaurav <gauravenigma@gmail.com>
2024-03-02 05:40:08 +0000

    expense.conf added

--- frontend.sh
+++ frontend.sh
@@ -3,6 +3,7 @@ dnf install nginx -y
 systemctl enable nginx
 systemctl start nginx
 
+ cp expense.conf etc/nginx/default.d/expense.conf
 rm -rf /usr/share/nginx/html/*
 
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
