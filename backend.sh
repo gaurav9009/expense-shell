ff09014096e48ff20957dd8a3c73a415a74183fc
Gaurav <gauravenigma@gmail.com>
2024-03-04 13:42:30 +0000

    Adding comments on  frontend.sh

--- backend.sh
+++ backend.sh
@@ -1,9 +1,14 @@
+
+#Install NodeJS
 dnf module disable nodejs -y
 dnf module enable nodejs:20 -y
 dnf install nodejs -y
+
+#Add application User
 useradd expense
 cp backend.service /etc/systemd/system/backend.service
 
+#Lets setup an app directory.
 mkdir /app
 
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip
